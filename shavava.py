b448c59f63667efadd8962223ecee9e8e597567f
IGORVIHORKOV2004 <Miroslavsibanov8@gmail.com>
2025-01-26 10:40:50 +0300

    COMIT 1.0 VSLOM

---
+++ shavava.py
@@ -0,0 +1,11 @@
+def argumen(x,y):
+    if y>0 and x<0:
+        print("II")
+    elif y>0 and x>0:
+        print("I")
+    elif y<0 and x<0:
+        print("III")
+    else:
+        print("IV")
+    #return argumen
+argumen(200,-20)
